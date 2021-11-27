8508fbb5bfb4678aad31d80eb3106fe9ea9190a7
Dylan Wiafe <dylanwiafe@gmail.com>
2021-11-27 07:20:40 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let cardOne = document.getElementById("cardOne");
+cardOne.addEventListener("click", (e) => {
+  //   alert(e.target.innerText);
+  console.log("#e3f2fd");
+});
+
+let cardTwo = document.getElementById("cardTwo");
+cardTwo.addEventListener("click", (e) => {
+  console.log("BBDEFB");
+});
+
+let cardThree = document.getElementById("cardThree");
+cardThree.addEventListener("click", (e) => {
+  //   alert(e.target.innerText);
+  console.log("#90caf9");
+});
+
+let cardFour = document.getElementById("cardFour");
+cardFour.addEventListener("click", (e) => {
+  //   alert(e.target.innerText);
+  console.log("#64B5F6");
+});
+
+let cardFive = document.getElementById("cardFive");
+cardFive.addEventListener("click", (e) => {
+  //   alert(e.target.innerText);
+  console.log("#42A5F5");
+});
